d1e2190c4a1b40220c6267fc3a5c7d2ba8d02323
Adarian Eastman <adarian@imginconline.com>
2016-01-29 10:28:44 -0500

    Fixed the missing src folder spooky problem

---
+++ src/globals.js
@@ -0,0 +1,11 @@
+//globals.js
+/*
+*	# noinfopath-sync
+*	@version 1.0.5
+*
+*	## Overview
+*	Provides data synchronization services.
+*/
+(function(angular){
+	angular.module("noinfopath.sync", []);
+})(angular);
